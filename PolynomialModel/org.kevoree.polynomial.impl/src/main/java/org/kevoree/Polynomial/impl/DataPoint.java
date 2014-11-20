f4f98f7f74e0e287b1d81a31375c46001ae15191
Assaad Moawad <assaad.moawad@uni.lu>
2014-11-20 13:06:18 +0100

    Cleanup

--- PolynomialModel/org.kevoree.polynomial.impl/src/main/java/org/kevoree/Polynomial/impl/DataPoint.java
+++
@@ -1,9 +0,0 @@
-package org.kevoree.Polynomial.impl;
-
-/**
- * Created by assaa_000 on 19/11/2014.
- */
-public class DataPoint {
-    public long time;
-    public double value;
-}
--- PolynomialModel/org.kevoree.polynomial.model/Polynomial.mm
+++
@@ -1,3 +0,0 @@
-class polynomial.sensor{
-   value : Double
-}
